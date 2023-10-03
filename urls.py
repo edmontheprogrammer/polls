ade8168f2c599fd57d3abf81619bee4f053d9790
Edmon Sebit <edmontheprogrammer@gmail.com>
2023-10-03 11:10:39 -0700

    Initial commit

---
+++ urls.py
@@ -0,0 +1,14 @@
+from django.urls import path
+
+from . import views
+
+app_name = 'polls'
+urlpatterns = [
+    path('', views.IndexView.as_view(), name='index'),
+    path('<int:pk>/', views.DetailView.as_view(), name='detail'),
+    path('<int:pk>/results/', views.ResultsView.as_view(), name='results'),
+    path('<int:question_id>/vote/', views.vote, name='vote'),
+    path('about/', views.about, name='about'),
+    path('features/', views.features, name='features'),
+    path('contact/', views.contact, name='contact'),
+]
